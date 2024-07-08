7da38d08cdbef38662265d038cd190c1c3e31a5f
Dokuyaku <sk8baz@gmail.com>
2024-07-08 23:31:33 +0200

    add last image

--- src/components/component/main-page.tsx
+++ src/components/component/main-page.tsx
@@ -110,15 +110,12 @@ export function MainPage() {
                 </Link>
               </div>
               <div>
-              <video
-                  src="/customer.mp4"
-                  width="80%"
-                  height="auto"
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                  className="rounded-lg"
+              <img
+                  src="/intro.jpeg"
+                  width={600}
+                  height={400}
+                  alt="TAUPINBAT"
+                  className="rounded-lg object-cover"
                 />
               </div>
             </div>
@@ -329,45 +326,6 @@ function FolderIcon(props: any) {
   );
 }
 
-function MailIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="20" height="16" x="2" y="4" rx="2" />
-      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-    </svg>
-  );
-}
-
-function MapPinIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
-      <circle cx="12" cy="10" r="3" />
-    </svg>
-  );
-}
 
 function MenuIcon(props: any) {
   return (
@@ -390,24 +348,7 @@ function MenuIcon(props: any) {
   );
 }
 
-function PhoneIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-    </svg>
-  );
-}
+
 
 function RefrigeratorIcon(props: any) {
   return (
